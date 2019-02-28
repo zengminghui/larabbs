ea3012922f9433fc40da803acca3db2fbb028347
Danny <531194759@qq.com>
2019-02-28 08:58:22 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
@@ -12,4 +14,17 @@ class UsersController extends Controller
     {
     	return view('users.show', compact('user'));
     }
+    public function edit(User $user)
+    {
+    	return view('users.edit',compact('user'));
+    }
+
+	/** 修改保存 **/
+	public function update(UserRequest $request, User $user)
+	{
+		// dd($request->all());die;
+	    $user->update($request->all());
+	    // Db::table('users')->where('id',$user->id)->update($request->all());
+	    return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+	}
 }
