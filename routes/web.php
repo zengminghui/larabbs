4db11ba17b1ee4fdb0448a4440ca85d3d9ce92ef
Danny <531194759@qq.com>
2019-03-02 10:28:36 +0000

    话题分类

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+/** 根据分类列表话题 **/
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
