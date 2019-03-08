9325a8c410850c5bf4b57e9b627e4a4ef21e4083
Danny <531194759@qq.com>
2019-03-08 07:31:28 +0000

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -28,4 +28,10 @@ class TopicObserver
             dispatch(new TranslateSlug($topic));
         }
     }
+
+    /** 删除话题并且删除回复**/
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+    }
 }
