1b4cbcfb51704b4a3ba7265a2acdaaa7da654bcd
Danny <531194759@qq.com>
2019-03-08 09:01:59 +0000

    集成 laravel-permission 扩展包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -49,5 +49,12 @@ class UsersTableSeeder extends Seeder
         $user->avatar = 'http://larabbs.test/uploads/images/avatars/201902/28/1_1551350350_DyGoZxIVvw.jpg';
         $user->save();
 
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
+
     }
 }
